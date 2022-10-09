1a0828bdcfa391f644ff0e7632ccd5e45039d67e
Lancer <572117235@qq.com>
2022-10-09 19:07:58 +0800

    Layout menu feature

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,6 +2,7 @@ import {Breadcrumb, Layout} from 'antd';
 import React from 'react';
 import {Outlet} from "react-router-dom";
 import Sidebar from "../sidebar/sidebar";
+import './layout.less';
 
 const {Header, Content, Footer} = Layout;
 
@@ -10,14 +11,15 @@ const LayoutWrapper: React.FC = () => {
 
     return (
         <Layout style={{minHeight: '100vh'}}>
-            <Sidebar />
+            <Sidebar/>
             <Layout className="site-layout">
-                <Header className="site-layout-background" style={{padding: 0}}/>
-                <Content style={{margin: '0 16px'}}>
-                    <Breadcrumb style={{margin: '16px 0'}}>
+                <Header className="site-layout-background" style={{padding: 0, display: 'flex'}}>
+                    <Breadcrumb style={{margin: '0 16px'}}>
                         <Breadcrumb.Item>User</Breadcrumb.Item>
                         <Breadcrumb.Item>Bill</Breadcrumb.Item>
                     </Breadcrumb>
+                </Header>
+                <Content style={{margin: '0 16px'}}>
                     <div className="site-layout-background" style={{padding: 24, minHeight: "100%"}}>
                         <Outlet/>
                     </div>
