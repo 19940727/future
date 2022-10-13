4070e3a1adb1a2fc9ec1a61dc3042cf41c854a9b
贺飞雷 <he.fl@asiainfo-sec.com>
2022-10-13 19:11:51 +0800

    提交菜单

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,6 +5,7 @@ import {
     DashboardOutlined,
     SettingOutlined,
     UserOutlined,
+    MenuOutlined,
 } from '@ant-design/icons';
 
 import Dashboard from "../pages/dashboard/dashboard";
@@ -12,6 +13,10 @@ import User from "../pages/system/user/user";
 import NoMatch from "../404";
 import LayoutWrapper from "../components/layout/layout";
 import Login from "../login";
+import Dept from "../pages/system/dept/dept";
+import Job from "../pages/system/job/job";
+import Role from "../pages/system/role/role";
+import Menu from "../pages/system/menu/menu";
 
 export declare type RouteConfig = RouteObject & {
     isMenu?: boolean;
@@ -46,6 +51,30 @@ let routes: RouteConfig[] = [
                         element: <User/>,
                         icon: <UserOutlined/>,
                         label: "用户管理",
+                    },
+                    {
+                        path: "/system/dept",
+                        element: <Dept/>,
+                        icon: <UserOutlined/>,
+                        label: "部门管理",
+                    },
+                    {
+                        path: "/system/job",
+                        element: <Job/>,
+                        icon: <UserOutlined/>,
+                        label: "岗位管理",
+                    },
+                    {
+                        path: "/system/role",
+                        element: <Role/>,
+                        icon: <UserOutlined/>,
+                        label: "角色管理",
+                    },
+                    {
+                        path: "/system/menu",
+                        element: <Menu/>,
+                        icon: <MenuOutlined />,
+                        label: "菜单管理",
                     },
                 ]
             },
